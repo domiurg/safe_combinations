5a490385d728cfc24f7eb47be8a069780093c19e
Maxim Domnitskyi <domiurg.93@gmail.com>
2017-07-25 14:12:07 +0800

    Updated comments; added illustration picture

--- safe.py
+++ safe.py
@@ -8,6 +8,7 @@ import pdb
 # p1 = p2 + b - c
 # from equation system above:
 # p3 - p1 = (b - a + 8) - (b - c)
+# p2 = p1 - (b - c)
 
 # Values of the First digit
 a = range(0, 10)
@@ -28,25 +29,29 @@ for i in a:
             # Check if it is viable
             if val > 9:
                 continue
-
             else:
-                # initialize list of possible solutions
+                # Initialize list of possible solutions
                 found = []
                 # 2 nested loops to find all combinations of p1 and p3 that satisfy equation
                 for p3 in range(0, 10):
                     for p1 in range(0, 10):
                         if (p3-p1) == val:
                             found.append((p1, p3))
-
+                # If solutions for p1 and p3 were found check further
                 if found:
-                    # if i == 3 and j == 2 and k == 1:
-                    #     pdb.set_trace()
                     for (p1, p3) in found:
+                        # Compute p2 from equation above
                         p2 = p1 - (j - k)
+                        # Check if p2 is viable
                         if p2 in range(0, 10):
+                            # Check for slots with opposite physical positions (Pic_1, Fig_#2)
                             if (p2 != p1 + 4) and (p3 != p2 + 5):
+                                # Count total number of solutions
                                 count += 1
+                                # Print to standard output
+                                # TODO: write to file
                                 print '(' + str(p1) + str(p2) + str(p3) + ')' + '(' + str(i) + str(j) + str(k) + ')'
+                                # Count total number of unique safe combinations
                                 if (i, j, k) not in unique:
                                     unique.append((i, j, k))
                 else:
